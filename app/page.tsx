cc112d3f7cca0127121d98eb1288a358bd1d89ac
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-05-27 23:51:24 +0300

    page deletions

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Hero />
         <Grid />
-        <RecentProjects />
-        <Clients />
         <Experience />
         <Approach />
-        <Footer />
       </div>
     </main>
   );
